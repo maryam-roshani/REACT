c5fbcac6a822a76000fcc7de5dca6df3e417181b
Maryam Roshani <maryamroshani777@gmail.com>
2024-05-29 02:09:14 +0430

    accordion

--- navbar with dropdown/src/components/accordion/accordion.jsx
+++ navbar with dropdown/src/components/accordion/accordion.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { useState } from 'react'
-import data from './accordionData';
+import React from 'react';
+import { useState } from 'react';
+import data ,{data2} from './accordionData';
 import { FaCaretDown, FaCaretUp } from "react-icons/fa6";
+import { data2 } from './accordionData'
 
 
 const Accordion = () => {
@@ -12,24 +13,28 @@ const Accordion = () => {
   return (
     <div className='p-3 w-64'>
         <div className="w-full p-2 rounded-md bg-slate-100" >
-            <div className='flex justify-between align-items-center' onClick={handleMenu}>
-                <p>Items</p>
-                {
-                    menu ? <FaCaretUp className='my-auto' /> : <FaCaretDown className='my-auto' />
+            {data2.map(item => (
+                <div className='flex justify-between align-items-center' onClick={handleMenu}>
+                    <p>Items</p>
+                    {
+                        menu ? <FaCaretUp className='my-auto' /> : <FaCaretDown className='my-auto' />
+                    }
+                </div>
+                {menu ? 
+                    <ul className='rounded-md pt-3'>
+                        {data.map(item => (
+                        <li
+                            key={item.id}
+                            className='p-1 hover:bg-slate-200 rounded-lg m-1 cursor-pointer duration-300 hover:text-black navItem text-sm text-slate-700'
+                        >
+                            {item.text}
+                        </li>
+                        ))}
+                    </ul>:<></>
                 }
-            </div>
-        </div>
-        {menu ? 
-        <ul className='items-list border-2 rounded-md pt-3'>
-            {data.map(item => (
-            <li
-                key={item.id}
-                className='p-1 hover:bg-slate-200 rounded-lg m-1 cursor-pointer duration-300 hover:text-black navItem text-sm text-slate-700'
-            >
-                {item.text}
-            </li>
             ))}
-        </ul>:<></>}
+        </div>
+        
     </div>
   )
 }
--- navbar with dropdown/src/components/accordion/accordionData.js
+++ navbar with dropdown/src/components/accordion/accordionData.js
@@ -5,4 +5,12 @@ const data = [
     { id: 4, text: 'subItem-4' }
 ]
 
+export const data2 = [
+    { id: 1, text: 'Item-1' },
+    { id: 2, text: 'Item-2' },
+    { id: 3, text: 'Item-3' },
+    { id: 4, text: 'Item-4' }
+]
+
+
 export default data
