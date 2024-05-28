13e52392fef1fbd778e655bb12adc2293223b14c
Maryam Roshani <maryamroshani777@gmail.com>
2024-05-29 01:32:47 +0430

    drop down

--- navbar with dropdown/src/components/Dropdown/Dropdown3.jsx
+++ navbar with dropdown/src/components/Dropdown/Dropdown3.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import data from './dropDownData.js'
 import girl from '../../assets/girl.jpg'
 import { useState } from 'react'
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 import './dropdown.css'
 const Dropdown = () => {
     const [menu, setMenu] = useState(false);
@@ -11,14 +12,19 @@ const Dropdown = () => {
       setMenu(!menu);
     };
   return (
-    <div>
-        <img src={girl} onClick={handleMenu}  alt="" className='w-16 h-16 rounded-full object-cover relative' />
+    <div className='p-3'>
+        <div className="w-64 p-2 flex justify-between rounded-md align-items-center bg-slate-100" onClick={handleMenu} >
+            <p>Items</p>
+            {
+                menu ? <FaAngleUp className='my-auto' /> : <FaAngleDown className='my-auto' />
+            }
+        </div>
         {menu ? 
-        <ul className='absolute items-list border-2 rounded-md top-20 pt-3'>
+        <ul className='absolute items-list border-2 rounded-md top-14  w-64 pt-3'>
             {data.map(item => (
             <li
                 key={item.id}
-                className='p-1 hover:bg-slate-200 rounded-xl m-1 cursor-pointer duration-300 hover:text-black navItem text-sm text-slate-700'
+                className='p-1 hover:bg-slate-200 rounded-lg m-1 cursor-pointer duration-300 hover:text-black navItem text-sm text-slate-700'
             >
                 {item.text}
             </li>
