9756e8927c61958614c7de2a83f02e6d08567be6
Maryam Roshani <maryamroshani777@gmail.com>
2024-05-31 18:45:44 +0430

    dash UI

--- Dash UI/src/App.jsx
+++ Dash UI/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import SocialAccounts from './components/social accounts/socialAccounts';
 import './App.css'
+import DangerZone from './components/danger zone/dangerZone';
 
 function App() {
   return (
     <>
       <SocialAccounts />
+      <DangerZone />
     </>
   )
 }
