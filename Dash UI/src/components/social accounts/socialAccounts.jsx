062eeadaf20622e30253ad9f15d660b4a809db98
Maryam Roshani <maryamroshani777@gmail.com>
2024-05-31 18:24:54 +0430

    dash UI

--- Dash UI/src/components/social accounts/socialAccounts.jsx
+++ Dash UI/src/components/social accounts/socialAccounts.jsx
@@ -4,19 +4,19 @@ import data from './socialData.js'
 const SocialAccounts = () => {
   return (
     <div className='p-5 bg-slate-50'>
-        <div className="bg-white rounded-md shadow-sm pt-5 px-4 pb-3 font-semibold">
+        <div className=" relative bg-white rounded-md shadow-sm pt-5 px-4 pb-12 font-semibold">
             <h3 className="text-xl font-bold text-red-950">Social accounts</h3>
             <p className="text-xs mb-3  text-slate-500">Let everyone know where they can find you.</p>
             <hr />
             <ul className='list-none'>
                 {data.map(item => 
                     <li key={item.id} className='flex justify-between p-3'>
-                        <h3 className="text-base text-slate-700">{item.socialNetwork}</h3>
+                        <h3 className="text-base text-slate-700 my-auto">{item.socialNetwork}</h3>
                         <input type="text" className="py-2 px-3 border-2 border-slate-200 rounded-md w-3/4 text-sm" placeholder={item.placeHolder}/>
                     </li>
                 )}
             </ul>
-            <button className='text-white rounded-md bg-blue-800 text-sm py-2 px-4 mt-3 float-end'>Save Social</button>
+            <button className='text-white rounded-md bg-blue-600 text-sm py-2 px-4 mt-3 absolute bottom-3 right-12'>Save Social</button>
         </div>
     </div>
   )
--- Dash UI/src/components/social accounts/socialData.js
+++ Dash UI/src/components/social accounts/socialData.js
@@ -2,9 +2,9 @@ const Data = [
     { id: 1, socialNetwork: "Twitter", placeHolder: "Add Twitter Name" },
     { id: 2, socialNetwork: "Facebook", placeHolder: "Add Facebook Name" },
     { id: 3, socialNetwork: "Instagram", placeHolder: "Add Instagram Name" },
-    { id: 4, socialNetwork: "Github", placeHolder: "Add Github Name" },
-    { id: 5, socialNetwork: "Youtube", placeHolder: "Add Youtube Name" },
-    { id: 6, socialNetwork: "Dribble", placeHolder: "Add Dribble Name" },
+    { id: 4, socialNetwork: "Github", placeHolder: "Github Account" },
+    { id: 5, socialNetwork: "Youtube", placeHolder: "Youtube" },
+    { id: 6, socialNetwork: "Dribble", placeHolder: "Dribble" },
 
 ]
 
