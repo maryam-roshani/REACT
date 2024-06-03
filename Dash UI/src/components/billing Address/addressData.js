6f3c58e83a7a3a38c1d0c7dc7900c41574a2e85c
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-04 02:48:50 +0430

    billing address

---
+++ Dash UI/src/components/billing Address/addressData.js
@@ -0,0 +1,10 @@
+const data = [
+    { id: 1, label: "Country", placeHolder: "Iran", type: "text", full: "true" },
+    { id: 2, label: "Address line 1", placeHolder: "Address line 1", type: "text", full: "true" },
+    { id: 3, label: "Address line 2", placeHolder: "Address line 2", type: "text", full: "true" },
+    { id: 4, label: "City", placeHolder: "Tehran", type: "text", full: "true" },
+    { id: 5, label: "State", placeHolder: "Tehran", type: "text", full: "false" },
+    { id: 6, label: "Zip/Postal Code", placeHolder: "0000", type: "number", full: "false" },
+
+
+]
