392b3378a904ff85b256258eafbcded07a3c2e62
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-07 08:19:03 +0430

    efficient

--- Dash UI/src/components/efficient/efficientData.js
+++ Dash UI/src/components/efficient/efficientData.js
@@ -1,10 +1,17 @@
+import arrow from '../../assets/arrow-counterclockwise.svg'
+import file from '../../assets/file-earmark-text.svg'
+import funnel from '../../assets/funnel.svg'
+import pencil from '../../assets/pencil-square.svg'
+import tag from '../../assets/tag.svg'
+import up_arrow from '../../assets/graph-up-arrow.svg'
+
 const data = [
-    { id: 1, name: 'Create and edit projects', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
-    { id: 2, name: 'search and filter', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
-    { id: 3, name: 'real time updates', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
-    { id: 4, name: 'meta information', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
-    { id: 5, name: 'pre-rendered results', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
-    { id: 6, name: 'simple analytics', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' }
+    { id: 1, logo: pencil, name: 'Create and edit projects', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
+    { id: 2, logo: funnel, name: 'search and filter', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
+    { id: 3, logo: arrow, name: 'real time updates', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
+    { id: 4, logo: tag, name: 'meta information', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
+    { id: 5, logo: file, name: 'pre-rendered results', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' },
+    { id: 6, logo: up_arrow, name: 'simple analytics', text: 'Lore,m ipsum dolor sit amet, consectetur adipiscing elit. Donec vel aliquam augue. Nulla rutrum lorem id lacus malesuada pretium. Aenean.' }
 ]
 
 export default data
