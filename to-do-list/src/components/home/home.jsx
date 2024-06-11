e632b77aa5c460b42310d3ece22c943f1352be10
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-12 01:02:02 +0430

    to-do-list home

--- to-do-list/src/components/home/home.jsx
+++ to-do-list/src/components/home/home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import data from './tasksData'
 
 const Home = () => {
   return (
     <div className="flex justify-content-center align-items-center bg-slate-100">
         <div className="rounded-md p-2 bg-white">
-          <ul>
-            {}
+          <ul className='list-none'>
+            {data.map(item => 
+                <li key={item.id} className='flex gap-2 rounded-md p-2'>
+                    <h4 className="text-base font-medium">{item.id}</h4>
+                    <h4 className="text-base font-medium">{item.priority}</h4>
+                    <h4 className="text-base font-medium">{item.status}</h4>
+                </li>
+            )}
           </ul>
         </div>
       </div>
