48764e249cd1519c8383a5a1b8f6a65f48fa784d
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-12 00:51:53 +0430

    to-do-list home

---
+++ to-do-list/src/components/home/tasksData.js
@@ -0,0 +1,7 @@
+const data = [
+    { id: 1, title: "task-1", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in tincidunt lacus, vitae mattis dolor. Duis nunc erat, blandit a.", priority: "high", status: "done", time_added: "" },
+    { id: 2, title: "task-2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in tincidunt lacus, vitae mattis dolor. Duis nunc erat, blandit a.", priority: "medium", status: "cancelled", time_added: "" },
+    { id: 3, title: "task-3", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in tincidunt lacus, vitae mattis dolor. Duis nunc erat, blandit a.", priority: "low", status: "in progress...", time_added: "" }
+]
+
+export default data
