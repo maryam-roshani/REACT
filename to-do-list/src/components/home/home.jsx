8b6f3e2043bcb6bf81d8f2e002a82213be830388
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-12 00:53:34 +0430

    to-do-list home

---
+++ to-do-list/src/components/home/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="flex justify-content-center align-items-center bg-slate-100">
+        <div className="rounded-md p-2 bg-white">
+          <ul>
+            {}
+          </ul>
+        </div>
+      </div>
+  )
+}
+
+export default Home
