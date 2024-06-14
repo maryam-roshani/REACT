ddb4fc005c078e762fe3803f63eb0c2413e2b151
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-15 02:26:11 +0430

    to-do-list loginSignup

--- to-do-list/src/components/loginSignup/loginSignupData.js
+++ to-do-list/src/components/loginSignup/loginSignupData.js
@@ -1,12 +1,12 @@
 const data1 = [
-    { id: 1, placeHolder: 'Your Name' },
-    { id: 2, placeHolder: 'Enter Email' },
-    { id: 3, placeHolder: 'Password' }
+    { id: 1, placeHolder: 'Your Name', type: 'text' },
+    { id: 2, placeHolder: 'Enter Email', type: 'email' },
+    { id: 3, placeHolder: 'Password', type: 'password' }
 ]
 
 export const data2 = [
-    { id: 1, placeHolder: 'Enter Email' },
-    { id: 2, placeHolder: 'Password' }
+    { id: 1, placeHolder: 'Enter Email', type: 'email' },
+    { id: 2, placeHolder: 'Password', type: 'password' }
 ]
 
 export default data1
