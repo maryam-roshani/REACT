b3423fde25a9245d7eaaca188f96c8b737836ffb
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-15 03:22:41 +0430

    to-do-list tasks

--- to-do-list/src/components/home/home.jsx
+++ to-do-list/src/components/home/home.jsx
@@ -17,9 +17,12 @@ const Home = () => {
     <div className="h-screen flex justify-content-center align-items-center bg-slate-100">
         <div className="rounded-md p-3 bg-white m-auto">
           <ul className='list-none'>
-            <li key={item.id} className='grid grid-cols-7 gap-4 rounded-md p-3'>
-                <TaskItem id = {item.id} title={item.title} priority={item.priority} status={item.status} />
-            </li>
+            <div className='grid grid-cols-7 gap-4 rounded-md p-3'>
+              <h3 className="text-lg font-medium col-span-1">ID</h3>
+              <h3 className="text-lg font-medium col-span-2">Title</h3>
+              <h3 className="text-lg font-medium col-span-2">Priority</h3>
+              <h3 className="text-lg font-medium col-span-2">Status</h3>
+            </div>
             {data.map(item => 
                 <li key={item.id} className='grid grid-cols-7 gap-4 rounded-md p-3'>
                     <TaskItem id = {item.id} title={item.title} priority={item.priority} status={item.status} />
