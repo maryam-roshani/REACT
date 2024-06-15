10c6820f7cc2902a0cc3f4b28d56c5fc293d1b67
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-15 21:35:45 +0430

    to-do-list taskDisplay

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -2,17 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import Home from './components/home/home'
 import LoginSignup from './components/loginSignup/LoginSignup'
-import Detail from './components/detail'
+import Detail from './components/TaskDisplay'
+import TaskCategory from './Pages/TaskCategory'
+import { BrowserRouter } from 'react-router-dom'
+
+
+
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       <div className='h-full'>
         < Home />
         <LoginSignup />
         <Detail />
       </div>
+      <Routes>
+        <Route path='/task' element={<Task/>}>
+          <Route path=':taskId' element={<Task/>}/>
+        </Route>
+      </Routes>
+
+      </BrowserRouter>
+      
     </>
   )
 }
