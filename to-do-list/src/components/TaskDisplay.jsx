f9f7de1a12682171f38ddbda496fdcb095b7637e
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-15 21:22:01 +0430

    to-do-list taskDisplay

--- to-do-list/src/components/TaskDisplay.jsx
+++ to-do-list/src/components/TaskDisplay.jsx
@@ -4,7 +4,9 @@ import { TaskContext } from '../context/TaskContext';
 const TaskDisplay = (props) => {
     const {task} = useContext(TaskContext);
     return (
-        <div className="w-96 h-48 rounded-md"></div>
+        <div className="w-96 h-48 rounded-md bg-white p-5">
+            <h3 className="text-xl font-medium mb-5 text-center">{task.title}</h3>
+        </div>
     )
 }
 
