f4c610c869857e8a0b82f70de9bc58b8e8fb6fb4
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-21 17:05:52 +0430

    to-do-list fixing errors

--- to-do-list/src/components/home/home.jsx
+++ to-do-list/src/components/home/home.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 const TaskProp = (props) => {
   return (
    <>
-      <h4 className="text-base font-medium col-span-1">{props.content}</h4>
+      <h4 className="text-base font-medium gap-7 col-span-1">{props.content}</h4>
    </>
   )
 }
@@ -14,7 +14,7 @@ const TaskProp = (props) => {
 const TaskHeader = (props) => {
   return (
     <>
-       <h3 className="text-lg font-medium col-span-1">{props.content}</h3>
+       <h3 className="text-lg font-medium col-span-1 cursor-pointer gap-7">{props.content}</h3>
     </>
   )
 }
@@ -37,7 +37,8 @@ const Home = () => {
   const narrowItems = (item) => {
     return item.filter === ""
       ? item
-      : item.id == filter | item.title.icludes(filter) | item.priority.includes(filter) | item.status.includes(filter) 
+      : item.id == filter | (typeof item.title == "string" && item.title.indexOf(filter) > -1)
+      | (typeof item.priority == "string" && item.priority.indexOf(filter) > -1) | (typeof item.status == "string" && item.status.indexOf(filter) > -1) 
   }
 
   const handleSort1 = () => {
@@ -63,21 +64,23 @@ const Home = () => {
   return (
     <div className="h-screen flex justify-content-center align-items-center bg-slate-100">
 
-        <div className="rounded-md p-3 bg-white m-auto">
-          <div className="block my-3 w-full p-2">
-            <input type="text" onChange={(e) => setFilter(e.target.value)} />
+        <div className="rounded-md p-3 bg-white m-auto w-2/3">
+          <div className="block my-3">
+            <input type="text" onChange={(e) => setFilter(e.target.value)} className='w-full p-2 border-slate-400 rounded-md bg-slate-50 text-lg' placeholder='search what you want ...'/>
           </div>
-          <ul className='list-none'>
-            <div className='grid grid-cols-7 gap-4 rounded-md p-3'>
+          <ul className='list-none w-full'>
+            <div className='grid grid-cols-4 rounded-md p-3 w-full'>
               <TaskHeader content="id" onClick = {handleSort1} />
                <TaskHeader content="title" onClick = {handleSort2} />
                <TaskHeader content="priority" onClick = {handleSort3} />
                <TaskHeader content="status" onClick = {handleSort4} />
             </div>
             {data.filter(narrowItems).map(item => 
-                <Link to={`/task/${item.id}`} key={item.id}><li key={item.id} className='grid grid-cols-7 gap-4 rounded-md p-3'>
+                <Link to={`/task/${item.id}`} key={item.id}>
+                  <li key={item.id} className='grid grid-cols-4 rounded-md p-3'>
                     <TaskItem id = {item.id} title={item.title} priority={item.priority} status={item.status} />
-                </li></Link>
+                  </li>
+                </Link>
             )}
           </ul>
         </div>
