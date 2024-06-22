644f7b399385453ce75d81d8eb7e49ce127f0316
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-22 18:41:22 +0430

    to-do-list navbar

--- to-do-list/src/components/navbar.jsx
+++ to-do-list/src/components/navbar.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="w-full h-24 bg-white p-2">
-        <h3 className="">Login</h3>
-        <h3 className="">Sign Up</h3>
-        <h3 className="">home</h3>
+    <div className="w-full h-16 bg-white p-2 flex align-items-center justify-content-between">
+        <h3 className="my-auto px-6">home</h3>
+        <div className="flex justify-around w-1/3 my-auto">
+            <h3 className="">Login</h3>
+            <h3 className="">Sign Up</h3>
+        </div>
     </div>
   )
 }
