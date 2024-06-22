e6e30d9f928cd13a178ebe1c0ffc721f43f6355c
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-22 18:25:08 +0430

    to-do-list navbar

---
+++ to-do-list/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-24 bg-white p-2">
+        <h3 className="">Login</h3>
+        <h3 className="">Sign Up</h3>
+        <h3 className="">home</h3>
+    </div>
+  )
+}
+
+export default Navbar
