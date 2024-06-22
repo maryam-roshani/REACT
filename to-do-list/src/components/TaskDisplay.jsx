8b16ac026c938cc69403f7ec9844edc04c380714
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-22 04:32:03 +0430

    to-do-list showing time

--- to-do-list/src/components/TaskDisplay.jsx
+++ to-do-list/src/components/TaskDisplay.jsx
@@ -6,13 +6,14 @@ import { useContext } from 'react';
 const TaskDisplay = (props) => {
     const {task} = props;
     const {timeDistance} = useContext(TaskContext)
+    const [currentDate, setCurrentDate] = useState(task.time_added);
     return (
         <div className="bg-slate-50 flex justify-content-center align-items-center w-full h-full">
             <div className="w-96 h-48 rounded-md bg-white p-5 m-auto relative">
                 <h3 className="text-xl font-medium mb-5 text-left ps-3">{task.title}</h3>
                 <p className="p-3 mt-3">{task.text}</p>
-                <span className=" absolute rotate-45 top-5 -right-11 text-lg trapezoid"><p className="top-2 left-4 absolute">{task.status}</p></span>
-                <p className="text-right p-3 my-2">{timeDistance}</p>
+                <span className=" absolute rotate-45 top-5 -right-11 text-lg trapezoid"><p className="top-2 left-4 absolute">{task.priority}</p></span>
+                <p className="text-right p-3 my-2">{currentDate}</p>
             </div>
         </div>
     )
