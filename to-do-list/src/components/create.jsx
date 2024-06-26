e7fa33a6b219f74ac8c96e4410fbc0955ea9bfbf
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-26 19:26:16 +0430

    to-do-list create

--- to-do-list/src/components/create.jsx
+++ to-do-list/src/components/create.jsx
@@ -7,15 +7,29 @@ const OptionSelect = (props) => {
 
 const Create = () => {
     const [value, setValue] = useState('')
+    const [formData, setFormData] = useState({})
+    const handleSubmit = (event) => { 
+      event.preventDefault(); 
+      const dataToSubmit = { 
+        ...formData 
+      };
+    }
     const options = [
        { label: "high", value: "high"},
        { label: "medium", value: "medium"},
        { label: "low", value: "low"}
     ]
-  
     function handleSelect(event) {
         setValue(event.target.value)
     }
+    const handleInputChange = (event) => { 
+        const { target } = event; 
+        const { name, value } = target;
+        setFormData({
+          ...formData, 
+          [name]: value 
+        }); 
+      }
 
     return (
         <div className="bg-slate-50 flex justify-content-center align-items-center w-full h-full">
@@ -35,43 +49,7 @@ const Create = () => {
                 </form>
            </div>
         </div>
-      )
-
-
-  const [formData, setFormData] = useState({})
-  const handleSubmit = (event) => { 
-    event.preventDefault(); 
-    const dataToSubmit = { 
-      ...formData 
-    };
-}
-
-   const handleInputChange = (event) => { 
-     const { target } = event; 
-     const { name, value } = target;
-     setFormData({
-       ...formData, 
-       [name]: value 
-     }); 
-   }
-  return (
-    <div className="bg-slate-50 flex justify-content-center align-items-center w-full h-full">
-        <div className="w-96 h-64 rounded-md bg-white p-5 m-auto relative" >
-            <form onSubmit={handleSubmit} className='block'>
-                <label htmlFor="my-input">Title</label> 
-                <input id="my-input" type="text" name="myInput" onChange={handleInputChange} className='p-3 w-full rounded-md' />
-                <select value={value} onChange={handleInputChange} className='block text-lg'>
-
-                <OptionSelect value= "high" />
-                <OptionSelect value= "medium" />
-                <OptionSelect value= "low" />
-
-                </select>
-                <button type="submit" className='block'>Submit</button>
-            </form>
-       </div>
-    </div>
-  )
+    )
 }
 
 export default Create
