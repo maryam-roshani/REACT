b06b912a393d1a5d4d4ee05e90526fc933fe4067
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-26 06:27:41 +0430

    to-do-list initial-tasks

--- to-do-list/src/components/home/home.jsx
+++ to-do-list/src/components/home/home.jsx
@@ -35,10 +35,20 @@ const TaskItem = (props) => {
     </>
   )
 }
+const d1 = new Date(2024, 5, 10, 10, 33)
+const d2 = new Date(2024, 5, 15, 22, 23)
+const d3 = new Date(2024, 5, 14, 12, 33)
+
+let Initial_Tasks = [
+    { id: 1, title: "task-1", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in tincidunt lacus, vitae mattis dolor. Duis nunc erat, blandit a.", priority: "high", status: "done", time_added: d1 },
+    { id: 2, title: "task-2", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in tincidunt lacus, vitae mattis dolor. Duis nunc erat, blandit a.", priority: "medium", status: "cancelled", time_added: d2 },
+    { id: 3, title: "task-3", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in tincidunt lacus, vitae mattis dolor. Duis nunc erat, blandit a.", priority: "low", status: "in progress...", time_added: d3 }
+]
+
 
 const Home = () => {
   const [filter, setFilter] = useState('')
-  // const [todos, setTodos] = useState(data);
+  const [todos, setTodos] = useState(Initial_Tasks);
 
   const handleDelete = (index) => {
     // setTodos(todos => todos.filter((item, i) => i !== index));
