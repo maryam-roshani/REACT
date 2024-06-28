32e30273c534bfcd9d89495fda52ee5ef716336a
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-29 02:40:25 +0430

    to-do-list moving create to home

--- to-do-list/src/components/home/home.jsx
+++ to-do-list/src/components/home/home.jsx
@@ -64,6 +64,30 @@ const Home = () => {
     setInputPriority(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputTitle || !inputText || !inputPriority) {
+      alert("fill data");
+    }
+    else {
+      const allInputData = {
+        id: ln+1,
+        title: inputTitle,
+        text: inputText,
+        priority: inputPriority,
+        status: "in progress...",
+        time_added: new Date(),
+      };
+      setTodos([allInputData, ...todos]);
+      console.log(todos)
+    }
+  }
+  const options = [
+     { label: "high", value: "high"},
+     { label: "medium", value: "medium"},
+     { label: "low", value: "low"}
+  ]
+
   const handleDelete = (id) => {
     setTodos(todos => todos.filter((item) => item.id !== id));
   };
@@ -118,6 +142,23 @@ const Home = () => {
           </ul>
           <Link to={`/task/create`}><button className="w-full p-3 text-lg bg-purple-700 text-zinc-50 rounded-lg mt-12">Add New Task</button></Link>
         </div>
+        <div className="w-96 h-64 rounded-md flex justify-content-center mt-5" >
+              <form onSubmit={handleSubmit} className='block'>
+                  <label htmlFor="my-input">Title</label> 
+                  <input id="my-input" type="text" name="Title" onChange={handleInputTitle} value={inputTitle} className='p-3 w-full rounded-md' />
+                  <label htmlFor="my-textarea">Text</label> 
+                  <textarea id="my-textarea" name="Text" onChange={handleInputText} value={inputText} className='p-3 w-full rounded-md' />
+                  <div className="w-1/2 p-3 rounded-md border-1">
+                      <h4>Priority</h4>
+                      <select name="Priority" onChange={handleInputPriority} value={inputPriority} className='block text-lg'>
+                          {options.map(option => (
+                              <option value={option.value}>{option.label}</option>
+                          ))}
+                      </select>
+                  </div>
+                  <button type="submit" className='block'>Submit</button>
+              </form>
+         </div>
       </div>
   )
 }
@@ -170,7 +211,7 @@ const Create = () => {
 
   return (
       <div className="bg-slate-50 flex justify-content-center align-items-center w-full h-full">
-          <div className="w-96 h-64 rounded-md flex justify-content-center mt-5" >
+         <div className="w-96 h-64 rounded-md flex justify-content-center mt-5" >
               <form onSubmit={handleSubmit} className='block'>
                   <label htmlFor="my-input">Title</label> 
                   <input id="my-input" type="text" name="Title" onChange={handleInputTitle} value={inputTitle} className='p-3 w-full rounded-md' />
