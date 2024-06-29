493e62a38292685988ab5dfe2f93d56c8f2c6f7d
Maryam Roshani <maryamroshani777@gmail.com>
2024-06-30 04:13:39 +0430

    to-do-list conditional styling

--- to-do-list/src/components/TaskDisplay.jsx
+++ to-do-list/src/components/TaskDisplay.jsx
@@ -10,10 +10,10 @@ const TaskDisplay = (props) => {
 
     return (
         <div className="bg-slate-50 flex justify-content-center align-items-center w-full h-full">
-            <div className="w-96 h-64 rounded-md bg-white p-5 m-auto relative" style={{ backgroundColor: task.status == "done" ? "#7dd3fc !important" : (task.status == "cancelled" ? "#dfe6ed !important" : "") }} >
+            <div className="w-96 h-64 rounded-md bg-white p-5 m-auto relative" style={{ backgroundColor: task.status == "done" ? "#7dd3fc !important" : (task.status == "cancelled" ? "#dfe6ed !important" : "white") }} >
                 <h3 className="text-xl font-medium mb-5 text-left ps-3">{task.title}</h3>
                 <p className="p-3 mt-3">{task.text}</p>
-                <span className= "absolute rotate-45 top-5 -right-11 text-lg trapezoid" style={{ borderBottomColor: task.priority == "high" ? " rgb(23, 130, 23) !important" : (task.priority == "medium" ? "rgb(162, 162, 27) !important" : "") }}><p className="top-2 left-4 text-white absolute">{task.priority}</p></span>
+                <span className= "absolute rotate-45 top-5 -right-11 text-lg trapezoid" style={{ borderBottomColor: task.priority == "high" ? " rgb(23, 130, 23) !important" : (task.priority == "medium" ? "rgb(162, 162, 27) !important" : "red") }}><p className="top-2 left-4 text-white absolute">{task.priority}</p></span>
                 <p className="right-0 p-3 my-2 text-xs bottom-0 absolute">{timeDistance(time)}</p>
             </div>
         </div>
