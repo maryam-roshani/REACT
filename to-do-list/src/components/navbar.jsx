4e11a21448ceb2f38ed2868917114a9b7a6a5b51
Maryam Roshani <maryamroshani777@gmail.com>
2024-07-05 08:39:40 +0430

    navbar styling

--- to-do-list/src/components/navbar.jsx
+++ to-do-list/src/components/navbar.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="w-full h-12 bg-slate-300 p-2 flex align-items-center justify-content-between">
+    <div className="w-full h-12 bg-white text-slate-700 p-2 flex align-items-center justify-between shadow-md shadow-black">
         <Link to={`/`}><h3 className="my-auto px-6 text-lg font-semibold">home</h3></Link>
-        <div className="flex my-auto justify-conten-right">
+        <div className="flex my-auto justify-between gap-6 px-5">
         <Link to={`/login`}><h3 className=" text-lg font-semibold">Login</h3></Link>
         <Link to={`/signup`}><h3 className=" text-lg font-semibold">Sign Up</h3></Link>
         </div>
