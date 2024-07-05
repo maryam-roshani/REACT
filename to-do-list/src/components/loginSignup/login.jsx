e34774ab0ce2491efba2b8a6527ec8c429da3104
Maryam Roshani <maryamroshani777@gmail.com>
2024-07-05 09:04:16 +0430

    login page inputs

--- to-do-list/src/components/loginSignup/login.jsx
+++ to-do-list/src/components/loginSignup/login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
             <h1 className='mb-3 mx-1 text-xl font-semibold '>Login</h1>
               <form className='grid gap-3'>
                 <div className="grid gap-7">
-                  <ExampleInput type = "email" name= "email" value= {inputField.email} onChange= {inputsHandler}/>
-                  <ExampleInput type = "password" name= "password" value= {inputField.password} onChange= {inputsHandler} />
+                  <ExampleInput type = "email" name= "email" value= {inputEmail} onChange= {emailHandler}/>
+                  <ExampleInput type = "password" name= "password" value= {inputPassword} onChange= {passwordHandler} />
                   <div className="">Lost Password? <span className='font-semibold text-xs cursor-pointer'>Click Here!</span></div>
                 </div>
                 <button className='border-none p-3 text-lg mb-2 bg-red-500 rounded-md font-semibold text-white' onClick={handleSubmit}>Continue</button>
