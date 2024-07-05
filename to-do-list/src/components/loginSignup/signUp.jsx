12d86e8bccf1a52047cbfd80f60c57e54ff316a6
Maryam Roshani <maryamroshani777@gmail.com>
2024-07-05 09:14:23 +0430

    signup states

--- to-do-list/src/components/loginSignup/signUp.jsx
+++ to-do-list/src/components/loginSignup/signUp.jsx
@@ -15,10 +15,14 @@ const Signup = () => {
     const inputsHandler = (e) =>{
         setInputField( {[e.target.name]: e.target.value} )
     }
-
-    const submitButton = () =>{
-        alert(inputField)
-    }    
+    
+    const submitButton = (e) =>{
+      e.preventDefault();
+      const text = "email : " + inputEmail
+      const text1 = "password : " + inputPassword
+      console.log(text + '\n' + text1)
+      alert(text + '\n' + text1)
+  }    
   
     return (
         <div className="p-6 bg-blue-400">
@@ -27,9 +31,9 @@ const Signup = () => {
             <h1 className='mb-3 mx-1 text-xl font-semibold '>Sign Up</h1>
               <form className='grid gap-3'>
                 <div className="grid gap-7">
-                    <ExampleInput type = "text" name= "name" value= {inputField.name} onChange= {inputsHandler}/>
-                    <ExampleInput type = "email" name= "email" value= {inputField.email} onChange= {inputsHandler}/>
-                    <ExampleInput type = "password" name= "password" value= {inputField.password} onChange= {inputsHandler} />
+                    <ExampleInput type = "text" name= "name" value= {inputName} onChange= {nameHandler}/>
+                    <ExampleInput type = "email" name= "email" value= {inputEmail} onChange= {emailHandler}/>
+                    <ExampleInput type = "password" name= "password" value= {inputPassword} onChange= {passwordHandler} />
                 </div>
                 <button className='border-none p-3 text-lg mb-2 bg-red-500 rounded-md font-semibold text-white' onClick={submitButton}>Continue</button>
                 <p className="">Already have an account? <Link to={`/login`}><span className='font-bold text-red-600 text-lg cursor-pointer' >Login </span></Link>Here</p>
